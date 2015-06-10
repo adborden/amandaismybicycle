3b3e43a4b36eb59f27b46438abb973ede8198523
amandaxo <amandaxo@users.noreply.github.com>
2015-06-10 14:09:54 -0700

    new phrases added 6/10

--- js/phrases.js
+++ js/phrases.js
@@ -4,6 +4,9 @@ var PHRASES = [
   'Lick yourself today.',
   'Your mom told me to tell you hello.',
   'They think you are cute.',
-  'Bookmark your website.'
-
+  'Bookmark your website.',
+  'Catch slowpokes on your tongue.',
+  'Slowly, until the sun rises.',
+  'Make the just move.',
+  'Ignore the clown.'
 ];
